3e8d48dfd86f55ab50c9e9453c08298f77e04dbe
Kenneth Olsen <olsenkenneth1986@gmail.com>
2019-07-14 16:42:48 -0400

    Added user and exercise Schemas, Added Routes for users

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,12 @@ connection.once("open", () => {
     console.log("MongoDB database connection established successfully");
 })
 
+const exercisesRouter = require("./routes/exercises.js");
+const usersRouter = require("./routes/users.js");
+
+app.use("/exercises", exercisesRouter);
+app.use("/users", usersRouter);
+
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
